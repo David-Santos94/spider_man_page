6df7af38855e35d4845d561b094d69f6cb703afb
David Marlon dos Santos <david.marlon94@gmail.com>
2024-07-14 15:05:20 +0100

    sections characters and gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    const img = document.querySelectorAll('[data-img]');
+
+    for (let i = 0; i < img.length; i++){
+        const image = img[i].dataset.img;
+        const description = document.querySelector(`[data-description=${image}]`);
+        img[i].addEventListener('mouseenter', function() {
+            description.classList.remove('characters__container__photo__item__img__description--is-hidden');
+        })
+        img[i].addEventListener('mouseout', function() {
+            
+            description.classList.add('characters__container__photo__item__img__description--is-hidden');
+        })
+    }
 })

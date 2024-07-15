5243ba626a5c753cdceb94295da9e79eaade8817
David Marlon dos Santos <david.marlon94@gmail.com>
2024-07-15 22:48:34 +0100

    funcionamento carrousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const gallerySection = document.querySelector('.gallery');
+    const positionSection = gallerySection.getBoundingClientRect();
+    const slides = document.querySelectorAll('[data-slide]');
+    let count = 0;
+    let delay = 2000;
+    const positionNow = window.scrollY;
+    const calcPosition = positionSection.top + positionNow;
+
     const img = document.querySelectorAll('[data-img]');
+
+
+    function startCarrousel() {
+        const position = window.scrollY;
+        console.log(calcPosition, position);
+    
+        if (position > parseInt(calcPosition)) {
+            carrousel();
+        }
+    };
+
+    function carrousel() {
+        slides[count].classList.remove('gallery__container__carrousel__slide--show');
+        
+        count++;
+        if (count >= slides.length) {
+            count = 0;
+        }
+        
+        slides[count].classList.add('gallery__container__carrousel__slide--show');
+    };
+
+    setInterval(() => {
+        startCarrousel();
+    }, delay);
+
 
     for (let i = 0; i < img.length; i++){
         const image = img[i].dataset.img;
